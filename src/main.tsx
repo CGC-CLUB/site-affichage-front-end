714e3340701ee351aa11ce08eb55bd7c011a5ab0
Xenos <acristyano22@gmail.com>
2024-09-03 20:05:12 +0100

    setup `react-query`

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as BR } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BR>
-      <App />
-    </BR>
+    <QueryClientProvider client={queryClient}>
+      <BR>
+        <App />
+      </BR>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+    </QueryClientProvider>
   </StrictMode>,
 );
