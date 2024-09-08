56e9dc1b1ca7151aea2378d07d759d9c8fb26e30
Xenos <acristyano22@gmail.com>
2024-09-08 19:05:51 +0100

    add `add user` and `add post` modals

--- src/pages/dashboard/Posts.tsx
+++ src/pages/dashboard/Posts.tsx
@@ -1,3 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Posts() {
-  return <div>Posts</div>;
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild className="absolute right-5 top-5">
+          <Button>Add New Post</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add New Post</DialogTitle>
+            <DialogDescription>
+              fill-full this form to add a new post
+            </DialogDescription>
+          </DialogHeader>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" className="border border-slate-400" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="post-content">Post</Label>
+              <Textarea id="post-content" className="border border-slate-400" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
--- src/pages/dashboard/Users.tsx
+++ src/pages/dashboard/Users.tsx
@@ -1,3 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { DialogHeader, DialogFooter } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+
 export default function Users() {
-  return <div>Users</div>;
+  return (
+    <div>
+      <Dialog>
+        <DialogTrigger asChild className="absolute right-5 top-5">
+          <Button>Add New User</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add New User</DialogTitle>
+            <DialogDescription>
+              fill-full this form to add a new user
+            </DialogDescription>
+          </DialogHeader>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="first_name">First Name</Label>
+              <Input id="last_name" className="border border-slate-400" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="last_name">Last Name</Label>
+              <Input id="last_name" className="border border-slate-400" />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" className="border border-slate-400" />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
 }
