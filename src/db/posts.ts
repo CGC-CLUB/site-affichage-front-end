befd841abd81098251c8afb2bfdb20eccac08160
Xenos <acristyano22@gmail.com>
2024-09-23 13:59:24 +0100

    add testing data for users

--- src/db/posts.ts
+++ src/db/posts.ts
@@ -1,19 +1,12 @@
 // this file is only for testing purposes and does not included in the final version
 
 import { Post } from "@/types";
+import { users } from "./users";
 
 export const posts: Post[] = [
   {
     id: 1,
-    user: {
-      id: 1,
-      first_name: "John",
-      last_name: "Doe",
-      email: "john@example.com",
-      password: "password",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
+    user: users[0],
     content: "this is fake content",
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -22,15 +15,7 @@ export const posts: Post[] = [
   },
   {
     id: 2,
-    user: {
-      id: 2,
-      first_name: "Jane",
-      last_name: "Doe",
-      email: "jane@example.com",
-      password: "password",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
+    user: users[1],
     content: "this is fake content",
     createdAt: new Date(),
     updatedAt: new Date(),
@@ -39,15 +24,7 @@ export const posts: Post[] = [
   },
   {
     id: 3,
-    user: {
-      id: 3,
-      first_name: "Bob",
-      last_name: "Doe",
-      email: "bob@example.com",
-      password: "password",
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
+    user: users[2],
     content: "this is fake content",
     createdAt: new Date(),
     updatedAt: new Date(),
---
+++ src/db/users.ts
@@ -0,0 +1,33 @@
+// this file is only for testing purposes and does not included in the final version
+
+import { User } from "@/types";
+
+export const users:User[] = [
+  {
+    id: 1,
+    first_name: "John",
+    last_name: "Doe",
+    email: "john@example.com",
+    password: "password",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 2,
+    first_name: "Jane",
+    last_name: "Doe",
+    email: "jane@example.com",
+    password: "password",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: 3,
+    first_name: "Bob",
+    last_name: "Doe",
+    email: "bob@example.com",
+    password: "password",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
