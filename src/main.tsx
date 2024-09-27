2343cc18baf6d22dbe533d6ecd269e9f19cb3857
Xenos <acristyano22@gmail.com>
2024-09-27 17:44:42 +0100

    remove react-query and add apollo-client for gql queries

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as BR } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
+import { InMemoryCache, ApolloProvider, ApolloClient } from "@apollo/client";
 
-const queryClient = new QueryClient();
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <ApolloProvider client={client}>
       <BR>
         <App />
       </BR>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-    </QueryClientProvider>
+    </ApolloProvider>
   </StrictMode>,
 );
